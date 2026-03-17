bbd521c611cdb9c1c6ab7f97dd8b6cca0ce55a51
SidickSino <sidickabdoulayesino1@gmail.com>
2026-03-17 08:35:40 +0200

    Add new app icons and integrate Nativewind for UI styling.

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,24 @@
-// import { useAuth } from "@clerk/clerk-expo";
-import { Redirect } from "expo-router";
+// // import { useAuth } from "@clerk/clerk-expo";
+// import { Redirect } from "expo-router";
 
-const Page = () => {
-  // const { isSignedIn } = useAuth();
+// const Page = () => {
+//   // const { isSignedIn } = useAuth();
 
-  // if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+//   // if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
 
-  return <Redirect href="/(auth)/welcome" />;
-};
+//   return <Redirect href="/(auth)/welcome" />;
+// };
 
-export default Page;
+// export default Page;
+
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
