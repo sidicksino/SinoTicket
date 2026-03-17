9f3fe2c81e24c80c35bc0da92a288ab11075bf14
SidickSino <sidickabdoulayesino1@gmail.com>
2026-03-18 01:04:16 +0200

    Introduce authentication flow with welcome, sign-up, and sign-in screens, and restructure tab navigation.

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const SignIn = () => {
+    return (
+        <View className='flex-1 items-center justify-center'>
+            <Text className='text-2xl font-bold'>SignIn</Text>
+        </View>
+    )
+}
+
+export default SignIn
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const SignUp = () => {
+    return (
+        <View className='flex-1 items-center justify-center'>
+            <Text className='text-2xl font-bold'>SignUp</Text>
+        </View>
+    )
+}
+
+export default SignUp
