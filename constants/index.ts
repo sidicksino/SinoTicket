9f9929df47c426e12fa6a44ec5b173744a423ab0
SidickSino <sidickabdoulayesino1@gmail.com>
2026-03-17 17:48:29 +0200

    add onboarding images and data, and update app.json favicon.

---
+++ constants/index.ts
@@ -0,0 +1,36 @@
+import onboarding1 from "@/assets/images/onboarding1.png";
+import onboarding2 from "@/assets/images/onboarding2.png";
+import onboarding3 from "@/assets/images/onboarding3.png";
+
+export const images = {
+    onboarding1,
+    onboarding2,
+    onboarding3,
+};
+export const onboarding = [
+    {
+        id: 1,
+        title: "Book tickets across Chad in seconds",
+        description:
+            "With SinoTicket, easily find and book bus, travel, and event tickets anytime, anywhere.",
+        image: images.onboarding1,
+    },
+    {
+        id: 2,
+        title: "All your trips in one place",
+        description:
+            "Manage your bookings, view schedules, and access your tickets directly from your phone.",
+        image: images.onboarding2,
+    },
+    {
+        id: 3,
+        title: "Fast, secure, and reliable",
+        description:
+            "Pay safely, receive instant confirmation, and travel with confidence using SinoTicket.",
+        image: images.onboarding3,
+    },
+];
+
+export const data = {
+    onboarding,
+};
