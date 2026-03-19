b0708edd7bc86d019c2d2e52d94b2021b97d655e
SidickSino <sidickabdoulayesino1@gmail.com>
2026-03-20 01:26:53 +0200

    Add dark status bar to the root layout.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { Syne_700Bold } from "@expo-google-fonts/syne";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 import "../global.css";
@@ -47,6 +48,7 @@ export default function RootLayout() {
           <Stack.Screen name="(auth)" />
           <Stack.Screen name="+not-found" />
         </Stack>
+        <StatusBar style="dark" />
       </ClerkLoaded>
     </ClerkProvider>
   );
